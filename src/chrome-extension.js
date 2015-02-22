be054f264347b0bda99a0371d9ef0250f7fc9c8c
Gojko Adzic <gojko@gojko.com>
2015-02-22 12:26:44 +0100

    prevent global namespace polution by packing extension inside an anonymous function space

--- src/chrome-extension.js
+++ src/chrome-extension.js
@@ -1,16 +1,14 @@
-/* global chrome, BugMagnet, window, XMLHttpRequest */
-(function () {
-	'use strict';
-	window.BugMagnet = window.BugMagnet || {};
-	var processConfig = function () {
-			var configText = this.responseText;
-			BugMagnet.processConfigText(configText, new BugMagnet.ChromeMenuBuilder());
-		},
-		loadConfig = function () {
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', chrome.extension.getURL('config.json'));
-			xhr.onload = processConfig;
-			xhr.send();
-		};
-	loadConfig();
-})();
+/* global chrome, BugMagnet, XMLHttpRequest */
+var processConfig = function () {
+		'use strict';
+		var configText = this.responseText;
+		BugMagnet.processConfigText(configText, new BugMagnet.ChromeMenuBuilder());
+	},
+	loadConfig = function () {
+		'use strict';
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', chrome.extension.getURL('config.json'));
+		xhr.onload = processConfig;
+		xhr.send();
+	};
+loadConfig();
